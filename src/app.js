2fbce72ae9ce4229aa95c80e30b077abea82c39d
julian19028 <121776944+julian19028@users.noreply.github.com>
2023-04-25 03:18:34 +0000

    pending js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let number = ["1", "2", "3", "4", "5", "6"];
+let icons = ["♦", "♥", "♠", "♣"];
+let colors = ["black", "red"];
+
+window.onload = function numbers(number) {
+  document.getElementById("number").innerText = Math.floor(
+    Math.random() * 6 + 1
+  );
+  console.log(numbers);
 };
